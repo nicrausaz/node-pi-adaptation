447b8c1b0b39e771398307c87b051a8773c32bf9
nicrausaz <n.crausaz99@gmail.com>
2017-02-18 13:18:02 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <ShowNotes></ShowNotes>
+  </div>
+</template>
+
+<script>
+import ShowNotes from './components/showNotes'
+
+export default {
+  name: 'app',
+  components: {
+    ShowNotes
+  }
+}
+</script>
+
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
