558b88834d8a7ecacea92c5d54612eb189bfbe13
nicrausaz <n.crausaz99@gmail.com>
2017-02-19 20:50:54 +0100

    base UI done

--- src/components/addNotes.vue
+++ src/components/addNotes.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="notesTable">
-    <select>
-        <option v-for="semestre in semestres">{{semestre.num}}</option>
-    </select>
-    <select>
+    <form id="addNoteForm">
+      <select>
+      <option v-for="semestre in semestres">{{semestre.num}}</option>
+      </select>
+      <select>
         <option v-for="branche in branches">{{branche.nom}}</option>
     </select>
-    <select>
+      <select>
         <option v-for="note in notes">{{note.num}}</option>
     </select>
+      <input type="submit" />
+    </form>
   </div>
 </template>
 
