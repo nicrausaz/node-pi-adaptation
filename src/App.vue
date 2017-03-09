5920b4028232a165e24e29a38e2d27f49f290fca
nicrausaz <n.crausaz99@gmail.com>
2017-03-09 17:05:11 +0100

    navbar is back

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,6 @@
         <li><a @click="showNoteEnabled = false">Ajouter des notes</a></li>
       </ul>
     </div>
-    <pre>{{showNoteEnabled}}</pre>
     <ShowNotes v-if="showNoteEnabled"></ShowNotes>
     <AddNotes v-else></AddNotes>
   </div>
@@ -41,5 +40,32 @@
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
+  }
+</style>
+<style scoped>
+  ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333;
+  }
+  
+  li {
+    float: left;
+  }
+  
+  li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    cursor: pointer;
+  }
+  /* Change the link color to #111 (black) on hover */
+  
+  li a:hover {
+    background-color: #111;
   }
 </style>
